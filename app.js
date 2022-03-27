aff7db307a2c23e21f0451e54ed69916a193c171
ismailhaque <ismailhaque2956@gmail.com>
2022-03-27 22:57:54 +0600

    project complate

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// get Element
 
 import Alert from "./src/Alert.js";
+import Storage from "./src/Storage.js";
 
-                                
+// get Element                                
 const devs_form = document.getElementById(`devs-form`);
 const massage =document.querySelector(`.massage`);
 
+// submit form and add data
 devs_form.addEventListener(`submit`, function(e) {
     e.preventDefault();
 
@@ -18,7 +19,41 @@ devs_form.addEventListener(`submit`, function(e) {
         massage.innerHTML = Alert.danger(`All feilds are requierd`);
     } else {
         massage.innerHTML = Alert.Success(`Data Submited`);
+        
+        Storage.set(`Devs`, data);
+        devs_form.reset();
+        getdata();
     }
-
-    console.log(data);
 })
+
+// show data
+getdata();
+function getdata() {
+
+    const dataget = document.querySelector(`#dataget`);
+    
+    let allData = Storage.get(`Devs`);
+    let datalist;
+    
+    allData.map((data, index) =>{
+
+        let { name, location, photo, phone, email} = data;
+
+        datalist += `<tr>
+        <td>${index+1}</td>
+        <td>${name}</td>
+        <td>${email}</td>
+        <td>${phone}</td>
+        <td>${location}</td>
+        <td><img style="width: 50px; height: 50px; object-fit: cover; border-radius: 50%;" src="${photo ? photo : `img/download.png`}" alt=""></td>
+        <td>
+            <button class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></button>
+            <button class="btn btn-info btn-sm"><i class="fas fa-edit"></i></button>
+            <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+        </td>
+    </tr>`;
+    })
+
+    dataget.innerHTML= datalist;
+    
+}
--- src/Storage.js
+++ src/Storage.js
@@ -1,11 +1,23 @@
 class Storage{
     
     static has (key){
-        return
+        return localStorage.getItem(key)? localStorage.getItem(key) : false;
+    }
 
+    static get(key){
+        return JSON.parse(this.has(key));
+    }
+    static set (key, data){
+        
+        let setData = [];
 
+        if (this.has(key)) {
+            setData = JSON.parse(this.has(key));
+        }
 
+        setData.push(data);
 
-
+        localStorage.setItem(key, JSON.stringify(setData));
     }
 }
+export default Storage;
